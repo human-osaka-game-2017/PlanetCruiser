ec2ab2a96565048474a651dd9b703cb926490471
shirapi <yusei29game@gmail.com@gmail.com>
2017-12-07 15:52:26 +0900

    加速度の追加 アイテム考察中

---
+++ PlanetCruiser/ScoreUp.cpp
@@ -0,0 +1,10 @@
+#include "ScoreUp.h"
+
+ScoreUp::ScoreUp(const D3DXVECTOR2& pos):
+	ObjectBase(pos)
+{
+}
+
+ScoreUp::~ScoreUp()
+{
+}
---
+++ PlanetCruiser/ScoreUp.h
@@ -0,0 +1,13 @@
+#ifndef SCOREUO_H
+#define SCOREUO_H
+
+#include <ObjectBase.h>
+
+class ScoreUp :public ObjectBase{
+public:
+	ScoreUp(const D3DXVECTOR2& pos);
+	virtual ~ScoreUp();
+
+
+};
+#endif
